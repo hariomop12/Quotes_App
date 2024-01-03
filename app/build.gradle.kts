7c922e12f26f7656da02d3474e5135e5c347d56e
mahimaoppppp <mahimaoppppp@gmail.com>
2024-01-03 15:50:15 +0530

    Your commit message here

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,11 @@ android {
 
 dependencies {
 
+
+    implementation ("androidx.compose.material3: material3: 1. 0. 0- alpha11")
+    implementation ("androidx.compose.ui: ui: 1. 0. 0- alpha11")
+    implementation ("androidx.compose.foundation: foundation: 1. 0. 0- alpha11")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -59,6 +64,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
